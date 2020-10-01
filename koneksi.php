3fb785f257d8379ad7151b6acfb1b8a792455065
rikaanrokhim <32533942+rikaanrokhim@users.noreply.github.com>
2020-10-01 11:27:35 +0700

    Update koneksi.php
    add description message error

--- koneksi.php
+++ koneksi.php
@@ -3,6 +3,7 @@ try {
 	$con = new PDO("mysql:host=127.0.0.1;dbname=pdo_demo", 'root', '');
 	$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOExceptioon $ex) {
+	// message error
 	die("Connection Failed! :" . $ex->getMessage());
 	
 }
